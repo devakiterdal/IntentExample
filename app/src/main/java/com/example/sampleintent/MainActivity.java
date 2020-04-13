9c66d0de644f1a6d6721fbbacb18c6c85abcb499
Devaki <terdaldevaki@gmail.com>
2020-04-13 09:20:01 +0530

    Example of Intent

---
+++ app/src/main/java/com/example/sampleintent/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.sampleintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity<b1> extends AppCompatActivity {
+
+    private Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = findViewById(R.id.b1);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity();
+            }
+        });
+    }
+    public void openActivity(){
+        Intent intent = new Intent(this,Activity_2.class);
+        startActivity(intent);
+
+    }
+
+
+}
